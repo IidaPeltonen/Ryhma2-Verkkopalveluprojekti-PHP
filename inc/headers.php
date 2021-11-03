91470c25329447cac865cd034c7d30ba795e3d68
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-03 17:20:07 +0200

    startti - iida loi pohjia

---
+++ inc/headers.php
@@ -0,0 +1,12 @@
+<?php
+header('Access-Control-Allow-Origin:' . $_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Accept, Content-Type', 'Access-Control-Allow-Header');
+header('Access-Control-Max-Age: 3600');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD']=== 'OPTIONS') {
+    return 0;
+}
+
