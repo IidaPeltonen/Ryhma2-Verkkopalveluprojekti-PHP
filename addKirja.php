f945a7301406c67c2868e97edc2fb61abd69250a
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-10 14:30:18 +0200

    add.php toimii -iida

--- addKirja.php
+++ addKirja.php
@@ -16,28 +16,29 @@ $kirjailija = filter_var($input->kirjailija,FILTER_SANITIZE_STRING);
 $vuosi = filter_var($input->vuosi,FILTER_SANITIZE_NUMBER_INT);
 $kieli = filter_var($input->kieli,FILTER_SANITIZE_STRING);
 $kustantaja = filter_var($input->kustantaja,FILTER_SANITIZE_STRING);
-$trnro = filter_var($input->trnro,FILTER_SANITIZE_NUMBER_INT);
+$trnimi = filter_var($input->trnimi,FILTER_SANITIZE_STRING);
 $kuvaus = filter_var($input->kuvaus,FILTER_SANITIZE_STRING);
 $hinta = filter_var($input->hinta,FILTER_SANITIZE_NUMBER_INT);
 $saldo = filter_var($input->saldo,FILTER_SANITIZE_NUMBER_INT);
 
 try {
     $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $query = $db->prepare('insert into kirja (kirjanimi, kirjailija, vuosi, kieli, kustantaja, trnro, kuvaus, hinta, saldo) values (:kirjanimi, :kirjailija, :vuosi, :kieli, :kustantaja, :trnro, :kuvaus, :hinta, :saldo)');
+    $query = $db->prepare('insert into kirja (kirjanimi, kirjailija, vuosi, kieli, kustantaja, trnimi, kuvaus, hinta, saldo) values (:kirjanimi, :kirjailija, :vuosi, :kieli, :kustantaja, :trnimi, :kuvaus, :hinta, :saldo)');
     $query->bindValue(':kirjanimi',$kirjanimi, PDO::PARAM_STR);
     $query->bindValue(':kirjailija',$kirjailija, PDO::PARAM_STR);
     $query->bindValue(':vuosi', $vuosi, PDO::PARAM_INT);
     $query->bindValue(':kieli',$kieli, PDO::PARAM_STR);
     $query->bindValue(':kustantaja',$kustantaja, PDO::PARAM_STR);
-    $query->bindValue(':trnro', $trnro, PDO::PARAM_INT);
+    $query->bindValue(':trnimi', $trnimi, PDO::PARAM_STR);
     $query->bindValue(':kuvaus',$kuvaus, PDO::PARAM_STR);
     $query->bindValue(':hinta', $hinta, PDO::PARAM_INT);
     $query->bindValue(':saldo', $saldo, PDO::PARAM_INT);
     $query->execute();
 
     header('HTTP/1.1 200 OK');
-    $data = array('kirjaid' => $db->lastInsertId(),'kirjanimi' => $kirjanimi, 'kirjailija' => $kirjailija, 'vuosi' => $vuosi, 'kieli' => $kieli, 'kustantaja' => $kustantaja, 'trnro' => $trnro,    'kuvaus' => $kuvaus, 'hinta' => $hinta, 'saldo' => $saldo);
+    $data = array('kirjaid' => $db->lastInsertId(),'kirjanimi' => $kirjanimi, 'kirjailija' => $kirjailija, 'vuosi' => $vuosi, 'kieli' => $kieli, 'kustantaja' => $kustantaja, 'trnimi' => $trnimi,    'kuvaus' => $kuvaus, 'hinta' => $hinta, 'saldo' => $saldo);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     header('HTTP/1.1 500 Internal Server Error');
