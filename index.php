b284051f549f07cfe929ddf0b5e058c22e55a3fb
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-10 11:40:19 +0200

    index hakee nyt kaikki tiedot kirja-taulusta -iida

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-print "tervetuloa kauppaan!";
-//plapllaapllaapllla
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
+$sql = "select * from kirja";
+$query = $db->query($sql);
+$results = $query->fetchAll(PDO::FETCH_ASSOC);
+header('HTTP/1.1 200 OK');
+print json_encode($results);
+
 ?>
