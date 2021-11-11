371fc3978b62a0d6520da6a3ae660175175ffb55
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-11 13:49:05 +0200

    ok

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
-$sql = "select * from kirja";
+$sql = "select * from asiakas";
+/* $sql2 = "select * from asiakas";
+$sql3 = "select * from tilaus"; */
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
