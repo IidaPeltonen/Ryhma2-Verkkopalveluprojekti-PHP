7f9c6805202d1acf76f447444679025b79878b48
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-11 12:55:55 +0200

    asiakkaan dellaus -iida

---
+++ deleteAsiakas.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Access-Control-Allow-Origin:' . $_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Accept, Content-Type', 'Access-Control-Allow-Header');
+header('Access-Control-Max-Age: 3600');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD']=== 'OPTIONS') {
+    return 0;
+}
+
+$input = json_decode(file_get_contents('php://input'));
+$asid = filter_var($input->asid,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $query = $db->prepare('delete from asiakas where asid=(:asid)');
+    $query->bindValue(':asid', $asid, PDO::PARAM_INT);
+    $query->execute();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('asid' => $asid);
+    print json_encode($data);
+} catch (PDOException $pdoex) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error' => $pdoex->getMessage());
+    print json_encode($error);
+}
+?>
