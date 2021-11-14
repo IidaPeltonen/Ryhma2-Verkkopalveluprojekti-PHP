f35d6f7981628fc3f4b51e8c7446e0f421a0121f
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-14 10:42:40 +0200

    ok

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
-$sql = "select * from kirja";
+$sql = "SELECT distinct kirja.kirjaid, kirjanimi,kirjailija, vuosi, kieli, kustantaja, trnimi,  kuvaus, hinta, saldo, kuva, SUM(kpl)
+FROM tilausrivi, kirja
+where kirja.kirjaid = tilausrivi.kirjaid
+GROUP by kirjaid
+ORDER BY SUM(kpl) DESC LIMIT 7";
+//$sql = "select * from kirja";
 //$sql = "select * from asiakas";
 //$sql = "select * from tilaus";
 $query = $db->query($sql);
