53acd6dc6ebfbc4ac29aa5647cc8e0cb8deffda1
jonim-git <jonimaki1@gmail.com>
2021-11-15 22:10:13 +0200

    Järjestysnumerointi top7-listaan

--- Top.php
+++ Top.php
@@ -4,14 +4,12 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
-$sql = "SELECT distinct kirja.kirjaid, kirjanimi,kirjailija, vuosi, kieli, kustantaja, trnimi,  kuvaus, hinta, saldo, kuva, SUM(kpl) AS SUM
-    FROM tilausrivi, kirja
-    where kirja.kirjaid = tilausrivi.kirjaid
-    GROUP by kirjaid
-    ORDER BY SUM(kpl) DESC LIMIT 7";
+$sql = "SELECT ROW_NUMBER() over(order by sum(kpl) desc ) as rownum, kirja.kirjaid, kirjanimi,kirjailija, vuosi, kieli, kustantaja, trnimi,  kuvaus, hinta, saldo, kuva, SUM(kpl) AS SUM
+FROM tilausrivi, kirja
+where kirja.kirjaid = tilausrivi.kirjaid
+GROUP by kirjaid
+ORDER BY SUM(kpl) desc LIMIT 7";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
 print json_encode($results);
-
-?>
