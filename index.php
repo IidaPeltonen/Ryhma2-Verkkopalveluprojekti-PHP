b51b6237a39fc4a17d4d6d2538779e0c464ac894
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-19 18:13:29 +0200

    funtioiden ja headereiden käyttöönottokoe

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
+require_once './inc/headers.php';
+require_once './inc/functions.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+$db= openDb();
 
-$db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
+selectAsJson($db, "select * from kirja");
+
+/* $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
 $sql = "select * from kirja";
 //$sql = "select * from asiakas";
 //$sql = "select * from tilaus";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
-print json_encode($results);
+print json_encode($results); */
 
 ?>
