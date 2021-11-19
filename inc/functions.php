a480961688b43702a874995e2a181440d17c71b2
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-19 18:51:12 +0200

    lisää kirotushäröjä

--- inc/functions.php
+++ inc/functions.php
@@ -7,18 +7,18 @@ function openDb(): object {
     $user = $ini['user'];
     $password = $ini['password'];
     $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
 }
 
-function selectAsJson(object $db, string $sql): void {
+function selectAsJson(object $db,string $sql): void {
     $query = $db->query($sql);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
     header('HTTP/1.1 200 OK');
     echo json_decode($results);
 }
 
-function executeInsert(object $db, string $sql): int {
+function executeInsert(object $db,string $sql): int {
     $query = $db->query($sql);
     return $db->lastInsertId();
 }
