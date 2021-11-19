05bd92e2589727c600bab979a5929de119e0bf09
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-19 18:28:40 +0200

    kirjotushäröjen korjausta

--- inc/headers.php
+++ inc/headers.php
@@ -2,16 +2,17 @@
 header('Access-Control-Allow-Origin:' . $_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Credentials:true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Accept, Content-Type', 'Access-Control-Allow-Header');
-header('Access-Control-Max-Age: 3600');
+header('Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header');
 header('Content-Type: application/json');
+header('Access-Control-Max-Age: 3600');
+
 
 if ($_SERVER['REQUEST_METHOD']=== 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-    header("Access-Control-Allow-Headers; {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     
     exit (0);
 }
