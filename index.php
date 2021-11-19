a589ca81e5ca3d74413a6b97a6b5c32abc506d25
jonim-git <jonimaki1@gmail.com>
2021-11-19 19:15:13 +0200

    jotain

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
-require_once './inc/headers.php';
+// require_once './inc/headers.php';
 require_once './inc/functions.php';
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-$db= openDb();
+// try {
+// $db= openDb();
+// selectAsJson($db, 'select * from kirja');
+// }
+// catch (PDOException $pdoex) {
+//     returnError($pdoex);
+// }
+// header('Access-Control-Allow-Origin: *');
+// header('Content-Type: application/json');
 
-selectAsJson($db, "select * from kirja");
-
-/* $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
+$db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8','root','');
 $sql = "select * from kirja";
-//$sql = "select * from asiakas";
-//$sql = "select * from tilaus";
+// // $sql = "select * from asiakas";
+// // $sql = "select * from tilaus";
 $query = $db->query($sql);
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
-print json_encode($results); */
+print json_encode($results);
 
 ?>
