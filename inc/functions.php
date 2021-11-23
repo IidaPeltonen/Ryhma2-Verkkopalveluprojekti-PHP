ef73df178b81833dc5f82ae1f38b8c80f34b936d
helmioo <81010386+helmioo@users.noreply.github.com>
2021-11-23 18:25:36 +0200

    siistimistä, testailua

--- inc/functions.php
+++ inc/functions.php
@@ -6,7 +6,7 @@ function openDb(): object {
     $database = $ini['database'];
     $user = $ini['user'];
     $password = $ini['password'];
-    $db = new PDO("mysql:host=$host;port=8889;dbname=$database;charset=utf8",$user,$password);
+    $db = new PDO("mysql:host=$host;port=3000;dbname=$database;charset=utf8",$user,$password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     return $db;
 }
