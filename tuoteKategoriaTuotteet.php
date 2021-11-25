9b5752759276723ed8542a9ddaa36544419219ff
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-25 20:20:12 +0200

    category korjattu

--- tuoteKategoriaTuotteet.php
+++ tuoteKategoriaTuotteet.php
@@ -8,13 +8,16 @@ $parameters = explode('/', $uri);
 $category_id = $parameters[1];
 
 try {
-    $db= openDb();
-    selectAsJson($db, 'select * from kirja where category_id = $category_id');
+    $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8', 'root', '');
+    
+    $sql = "select * from kirja where category_id = $category_id";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    print json_encode($results);;
     }
     catch (PDOException $pdoex) {
         returnError($pdoex);
     }
+
     
-
-
-
