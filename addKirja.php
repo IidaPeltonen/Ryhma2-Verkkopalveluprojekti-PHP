6ccc338f85957ab45b4878d601db376c6ab197e8
Ville-r <81159672+Ville-r@users.noreply.github.com>
2021-11-26 22:19:54 +0200

    addKirjaa muokattu

--- addKirja.php
+++ addKirja.php
@@ -8,26 +8,26 @@ $kirjailija = filter_var($input->kirjailija,FILTER_SANITIZE_STRING);
 $vuosi = filter_var($input->vuosi,FILTER_SANITIZE_NUMBER_INT);
 $kieli = filter_var($input->kieli,FILTER_SANITIZE_STRING);
 $kustantaja = filter_var($input->kustantaja,FILTER_SANITIZE_STRING);
-$trnimi = filter_var($input->trnimi,FILTER_SANITIZE_STRING);
 $kuvaus = filter_var($input->kuvaus,FILTER_SANITIZE_STRING);
 $hinta = filter_var($input->hinta,FILTER_SANITIZE_NUMBER_INT);
 $saldo = filter_var($input->saldo,FILTER_SANITIZE_NUMBER_INT);
+$category_id = filter_var($input->trnimi,FILTER_SANITIZE_STRING);
 
 try {
     $db= openDb();
-    $query = $db->prepare('insert into kirja (kirjanimi, kirjailija, vuosi, kieli, kustantaja, trnimi, kuvaus, hinta, saldo) values (:kirjanimi, :kirjailija, :vuosi, :kieli, :kustantaja, :trnimi, :kuvaus, :hinta, :saldo)');
+    $query = $db->prepare('insert into kirja (kirjanimi, kirjailija, vuosi, kieli, kustantaja, kuvaus, hinta, saldo, category_id) values (:kirjanimi, :kirjailija, :vuosi, :kieli, :kustantaja, :kuvaus, :hinta, :saldo, :category_id)');
     $query->bindValue(':kirjanimi',$kirjanimi, PDO::PARAM_STR);
     $query->bindValue(':kirjailija',$kirjailija, PDO::PARAM_STR);
     $query->bindValue(':vuosi', $vuosi, PDO::PARAM_INT);
     $query->bindValue(':kieli',$kieli, PDO::PARAM_STR);
     $query->bindValue(':kustantaja',$kustantaja, PDO::PARAM_STR);
-    $query->bindValue(':trnimi', $trnimi, PDO::PARAM_STR);
     $query->bindValue(':kuvaus',$kuvaus, PDO::PARAM_STR);
     $query->bindValue(':hinta', $hinta, PDO::PARAM_INT);
     $query->bindValue(':saldo', $saldo, PDO::PARAM_INT);
+    $query->bindValue(':category_id', $category_id, PDO::PARAM_STR);
     $query->execute();
     header('HTTP/1.1 200 OK');
-    $data = array('kirjaid' => $db->lastInsertId(),'kirjanimi' => $kirjanimi, 'kirjailija' => $kirjailija, 'vuosi' => $vuosi, 'kieli' => $kieli, 'kustantaja' => $kustantaja, 'trnimi' => $trnimi,    'kuvaus' => $kuvaus, 'hinta' => $hinta, 'saldo' => $saldo);
+    $data = array('kirjaid' => $db->lastInsertId(),'kirjanimi' => $kirjanimi, 'kirjailija' => $kirjailija, 'vuosi' => $vuosi, 'kieli' => $kieli, 'kustantaja' => $kustantaja, 'kuvaus' => $kuvaus, 'hinta' => $hinta, 'saldo' => $saldo, 'trnimi' => $trnimi);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
