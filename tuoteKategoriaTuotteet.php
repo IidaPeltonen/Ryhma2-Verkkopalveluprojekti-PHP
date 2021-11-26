a0fb75aa5799c0be10922de9b237c7253bf29486
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-26 16:36:10 +0200

    typo pois

--- tuoteKategoriaTuotteet.php
+++ tuoteKategoriaTuotteet.php
@@ -8,8 +8,7 @@ $parameters = explode('/', $uri);
 $category_id = $parameters[1];
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=kauppa;charset=utf8', 'root', '');
-    
+    $db= openDb();
     $sql = "select * from kirja where category_id = $category_id";
     $query = $db->query($sql);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
