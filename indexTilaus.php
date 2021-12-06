69da8a4c0d1f379cfe1f4b4a9fd521f2fb62d4c2
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-06 11:17:15 +0200

    testiä tilaukseen

--- indexTilaus.php
+++ indexTilaus.php
@@ -5,10 +5,13 @@ require_once 'inc/headers.php';
 
 try {
     $db = openDb();
-    selectAsJson($db, "select * from tilaus");
+    selectAsJson($db, "SELECT tilaus.tilausnro, tilaus.asid, asiakas.astunnus,asiakas.asetunimi, asiakas.assukunimi, tilaus.pvm, tilaus.tila, tilausrivi.kirjaid, kirja.kirjanimi, tilausrivi.kpl from tilaus,asiakas, tilausrivi,kirja where tilaus.asid = asiakas.asid AND tilaus.tilausnro = tilausrivi.tilausnro AND tilausrivi.kirjaid = kirja.kirjaid;");
     } catch (PDOException $pdoex) {
     returnError($pdoex);
     }
+
+    //tämä näyttäisi kaikki tilaukset, asiakkaat, rilausrivit ja kirjat yhdellä haulla
+    //SELECT tilaus.tilausnro, tilaus.asid, asiakas.astunnus,asiakas.asetunimi, asiakas.assukunimi, tilaus.pvm, tilaus.tila, tilausrivi.kirjaid, kirja.kirjanimi, tilausrivi.kpl from tilaus,asiakas, tilausrivi,kirja where tilaus.asid = asiakas.asid AND tilaus.tilausnro = tilausrivi.tilausnro AND tilausrivi.kirjaid = kirja.kirjaid;
 
     
 
