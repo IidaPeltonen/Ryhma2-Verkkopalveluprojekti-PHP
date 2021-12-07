10b9ed38a6c1185e3564a170f0d81b5ae85a8f31
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-07 20:44:43 +0200

    siivottu

--- indexTilaus.php
+++ indexTilaus.php
@@ -9,10 +9,3 @@ try {
     } catch (PDOException $pdoex) {
     returnError($pdoex);
     }
-
-    //tämä näyttäisi kaikki tilaukset, asiakkaat, rilausrivit ja kirjat yhdellä haulla
-    //SELECT tilaus.tilausnro, tilaus.asid, asiakas.astunnus,asiakas.asetunimi, asiakas.assukunimi, tilaus.pvm, tilaus.tila, tilausrivi.kirjaid, kirja.kirjanimi, tilausrivi.kpl from tilaus,asiakas, tilausrivi,kirja where tilaus.asid = asiakas.asid AND tilaus.tilausnro = tilausrivi.tilausnro AND tilausrivi.kirjaid = kirja.kirjaid;
-
-    
-
-
--- updateTilaus.php
+++ updateTilaus.php
@@ -5,7 +5,6 @@ require_once './inc/functions.php';
 $input = json_decode(file_get_contents('php://input'));
 $tilausnro = filter_var($input->tilausnro,FILTER_SANITIZE_NUMBER_INT);
 $asid = filter_var($input->asid,FILTER_SANITIZE_NUMBER_INT);
-//$pvm = filter_var($input->pvm,FILTER_SANITIZE_STRING);
 $tila = filter_var($input->tila,FILTER_SANITIZE_STRING);
 
 try {
