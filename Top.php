4d32bc69a684ed182e835aa2860f6b3703496040
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-10 15:30:47 +0200

    typoja kirjanimissä

--- Top.php
+++ Top.php
@@ -6,13 +6,13 @@ require_once './inc/functions.php';
 try {
     $db= openDb();
     selectAsJson($db, 
-    "SELECT ROW_NUMBER() over(order by sum(kpl) desc ) as rownum, 
+    'SELECT ROW_NUMBER() over(order by sum(kpl) desc ) as rownum, 
     kirja.kirjaid, kirjanimi,kirjailija, vuosi, kieli, kustantaja, kuvaus, hinta, 
     saldo, kuva, SUM(kpl) AS SUM
     FROM tilausrivi, kirja
     where kirja.kirjaid = tilausrivi.kirjaid
     GROUP by kirjaid
-    ORDER BY SUM(kpl) desc LIMIT 7");
+    ORDER BY SUM(kpl) desc LIMIT 7');
 }
 catch (PDOException $pdoex) {
     returnError($pdoex);
